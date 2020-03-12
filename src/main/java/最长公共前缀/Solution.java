24385c5f89baf9f6b8c1eadf74d0297e7bb1d168
Euthpic <490114771@qq.com>
2020-03-13 00:01:30 +0800

    最长公共前缀
    Signed-off-by: Euthpic <490114771@qq.com>

---
+++ src/main/java/最长公共前缀/Solution.java
@@ -0,0 +1,19 @@
+package 最长公共前缀;
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        if (strs.length == 1) {
+            return strs[0];
+        }
+        String ans = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(ans)!=0){
+                ans=ans.substring(0,ans.length()-1);
+            }
+        }
+        return ans;
+    }
+}
