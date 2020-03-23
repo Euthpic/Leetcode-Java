19de16485192c2e6523c97e333a27887435da49e
Euthpic <490114771@qq.com>
2020-03-23 21:53:18 +0800

    括号生成:回溯
    Signed-off-by: Euthpic <490114771@qq.com>

--- src/main/java/括号生成/Solution.java
+++ src/main/java/括号生成/Solution.java
@@ -1,13 +1,29 @@
 package 括号生成;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
+    ArrayList<String> output = new ArrayList<>();
+
     public List<String> generateParenthesis(int n) {
-    return null;
+        backtrace("", n, n);
+        return output;
+    }
+
+    private void backtrace(String pattern, int left, int right) {
+        if (left == 0 && right == 0) {
+            output.add(pattern);
+            return;
+        }
+        if (left > right) {
+            return;
+        }
+        if (left > 0) {
+            backtrace(pattern + "(", left - 1, right);
+        }
+        if (right > 0) {
+            backtrace(pattern + ")", left, right - 1);
+        }
     }
 
 
