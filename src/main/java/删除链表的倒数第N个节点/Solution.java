dd89c490af3d4a57692f5227c62f143b28d09d30
Euthpic <490114771@qq.com>
2020-03-23 19:04:00 +0800

    删除链表的倒数第N个节点
    Signed-off-by: Euthpic <490114771@qq.com>

--- src/main/java/删除链表的倒数第N个节点/Solution.java
+++ src/main/java/删除链表的倒数第N个节点/Solution.java
@@ -2,29 +2,37 @@ package 删除链表的倒数第N个节点;
 
 public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if (n == 0 || head == null) {
+        if (head == null) {
             return null;
         }
-        if (n == 1) {
+        if (n == 0) {
             return head;
         }
-        ListNode next = head.next;
-        while (next != null && n > 1) {
+        ListNode next = head;
+        while (next != null && n > 0) {
             next = next.next;
             n--;
         }
-        //链表长度小于n
-        if (next == null && n > 1) {
-            return null;
+        //如果n大于链表长度
+        if (next == null && n > 0) {
+            return head;
         }
-        while (next.next!=null){
-
+        if (next == null && n == 0) {
+            return head.next;
         }
+        ListNode pre = head;
+        ListNode after = head;
+        while (next != null) {
+            pre = after;
+            after = after.next;
+            next = next.next;
+        }
+        pre.next=after.next;
         return head;
     }
 
 
-    public class ListNode {
+    private static class ListNode {
         int val;
         ListNode next;
 
