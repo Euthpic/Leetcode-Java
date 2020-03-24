6db688a4585ecc41a5c8dc7d917e5d7ea89f169a
Euthpic <490114771@qq.com>
2020-03-24 18:06:22 +0800

    k个一组翻转链表
    Signed-off-by: Euthpic <490114771@qq.com>

---
+++ src/main/java/k个一组翻转链表/Solution.java
@@ -0,0 +1,48 @@
+package k个一组翻转链表;
+
+public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode end = dummy;
+        while (end.next != null) {
+            for (int i = 0; i < k; i++) {
+                end = end.next;
+                if (end == null) {
+                    return dummy.next;
+                }
+            }
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            reverse(start);
+            pre.next=end;
+            start.next=next;
+            pre = start;
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
