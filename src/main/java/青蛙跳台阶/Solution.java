0911ab34dda03526397a1617dfb3147db2d936b2
Euthpic <490114771@qq.com>
2020-06-24 14:26:44 +0800

    青蛙跳台阶
    Signed-off-by: Euthpic <490114771@qq.com>

---
+++ src/main/java/青蛙跳台阶/Solution.java
@@ -0,0 +1,21 @@
+package 青蛙跳台阶;
+
+public class Solution {
+    public int numWays(int n) {
+        if (n==0){
+            return 1;
+        }
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        int pre1=1;
+        int pre2=2;
+        int sum=pre1+pre2;
+        for (int i = 3; i <= n; i++) {
+            sum=(pre1+pre2)%1000000007;
+            pre1=pre2;
+            pre2=sum;
+        }
+        return sum;
+    }
+}
