c4f77f36a316e9dae4552d5c42405320f3f45f70
Euthpic <490114771@qq.com>
2020-06-24 14:48:21 +0800

    旋转数组的最小数字.二分查找
    Signed-off-by: Euthpic <490114771@qq.com>

---
+++ src/main/java/旋转数组的最小数字/Solution.java
@@ -0,0 +1,23 @@
+package 旋转数组的最小数字;
+
+public class Solution {
+    public int minArray(int[] numbers) {
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (numbers[mid] > numbers[right]) {
+                left = mid + 1;
+            } else if (numbers[mid] < numbers[right]) {
+                right = mid ;
+            } else {
+                right --;
+            }
+        }
+        return numbers[left];
+    }
+
+
+}
