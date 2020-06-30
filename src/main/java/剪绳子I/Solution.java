e78ae435854f4e3f30e795e3cefd6da1cefed9f8
Euthpic <490114771@qq.com>
2020-06-30 23:18:52 +0800

    剪绳子I
    Signed-off-by: Euthpic <490114771@qq.com>

---
+++ src/main/java/剪绳子I/Solution.java
@@ -0,0 +1,29 @@
+package 剪绳子I;
+
+public class Solution {
+    public int cuttingRope(int n) {
+        return dp(n);
+    }
+
+    private int dp(int n) {
+        if (n == 0 || n == 1) {
+            return 0;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        if (n == 3) {
+            return 2;
+        }
+        if (n == 4) {
+            return 4;
+        }
+        if (n == 5) {
+            return 6;
+        }
+        if (n == 6) {
+            return 9;
+        }
+        return 3 * dp(n - 3);
+    }
+}
