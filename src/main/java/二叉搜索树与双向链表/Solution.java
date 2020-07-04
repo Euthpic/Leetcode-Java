dbd220d89fd84bb04fb8f28c69ecfafa8f7da589
Euthpic <490114771@qq.com>
2020-07-04 22:09:29 +0800

    二叉搜索树与双向链表
    Signed-off-by: Euthpic <490114771@qq.com>

---
+++ src/main/java/二叉搜索树与双向链表/Solution.java
@@ -0,0 +1,54 @@
+package 二叉搜索树与双向链表;
+
+public class Solution {
+    Node pre;
+    Node head;
+
+    public Node treeToDoublyList(Node root) {
+        if (root==null){
+            return null;
+        }
+        dfs(root);
+        head.left = pre;
+        pre.right = head;
+        return head;
+    }
+
+    private void dfs(Node cur) {
+        if (cur == null) {
+            return;
+        }
+        dfs(cur.left);
+
+        if (pre == null) {
+            head = cur;
+        } else {
+            pre.right = cur;
+        }
+        cur.left = pre;
+        pre = cur;
+
+        dfs(cur.right);
+    }
+
+    private class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    }
+
+    ;
+}
