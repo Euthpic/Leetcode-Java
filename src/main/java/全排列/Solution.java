8325b3768ee15befc266ef63fafb23d6d774c773
Guangfa <chenguangfa@iplas.com.cn>
2022-01-12 00:18:44 +0800

    46-全排列

--- src/main/java/全排列/Solution.java
+++ src/main/java/全排列/Solution.java
@@ -6,7 +6,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ *  1.把数组看成两部分,中间offset是即将要填入的位置,左边是已经填过的数字,右边是可选的数字,
+ *    那么一直向右填数字就可以得到结果.问题就转化为如何维护这个数组.
+ *  2.offset向右移(在回溯里面相当于往下一层)之前,依次交换第一个元素(first)和后面的元素(逐渐自增的i),那么在下一层看来这个数组还是满足1的条件
+ *  3.回退到上一层之前把元素交换回来,那么可以保证上一层处理的都是同一个数组,只是要交换的下标(i)不同
+ */
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
         int len=nums.length;
@@ -30,4 +35,4 @@ public class Solution {
     }
 
 }
-//leetcode submit region end(Prohibit modification and deletion)
+
