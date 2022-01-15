a91574e9f1495b596771820aadbe7441b3d54c20
Guangfa <490114771@qq.com>
2022-01-16 01:48:04 +0800

    49-字母异位词分组

--- src/main/java/字母异位词分组/Solution.java
+++ src/main/java/字母异位词分组/Solution.java
@@ -7,9 +7,9 @@ public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> res=new HashMap<>();
         for (String str : strs) {
-            char[] cs=str.toCharArray();
-            Arrays.sort(cs);
-            String key=String.valueOf(cs);
+            char[] chars=str.toCharArray();
+            Arrays.sort(chars);
+            String key=String.valueOf(chars);
             List<String> list=res.getOrDefault(key,new ArrayList<>());
             list.add(str);
             res.put(key,list);
