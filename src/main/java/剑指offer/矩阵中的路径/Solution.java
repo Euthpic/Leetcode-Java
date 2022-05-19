42e4a7f7db17ef8acdfd72bf2810732e20bb9f1a
euthpic <490114771@qq.com>
2022-05-19 20:23:16 +0800

    剑指offer.矩阵中的路径

--- src/main/java/剑指offer/矩阵中的路径/Solution.java
+++ src/main/java/剑指offer/矩阵中的路径/Solution.java
@@ -1,5 +1,9 @@
 package 剑指offer.矩阵中的路径;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Solution {
 
     public boolean exist(char[][] board, String word) {
@@ -7,32 +11,32 @@ public class Solution {
             return false;
         }
         char[] words = word.toCharArray();
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, words, i, j, 0)) {
-                    return true;
-                }
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if (dfs(board, words, col, row, 0)) return true;
             }
         }
+
         return false;
     }
 
-    private boolean dfs(char[][] board, char[] words, int i, int j, int k) {
-        if (i < 0 || i >= board.length ||
-                j < 0 || j >= board[0].length ||
-                board[i][j] != words[k]) {
-            return false;
-        }
+    boolean dfs(char[][] board, char[] words, int col, int row, int k) {
+        if (col >= board[0].length || row >= board.length
+                || col < 0 || row < 0 || board[row][col] != words[k]
+        ) return false;
+
         if (k == words.length - 1) {
             return true;
         }
-        char tmp = board[i][j];
-        board[i][j] = '/';
-        boolean res = dfs(board, words, i + 1, j, k + 1) ||
-                dfs(board, words, i - 1, j, k + 1) ||
-                dfs(board, words, i, j + 1, k + 1) ||
-                dfs(board, words, i, j - 1, k + 1);
-        board[i][j] = tmp;
-        return res;
+        char tmp = board[row][col];
+        board[row][col] = '/';
+        int[][] directions = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        for (int[] direction : directions) {
+            if (dfs(board, words, col + direction[0], row + direction[1], k + 1)) {
+                return true;
+            }
+        }
+        board[row][col] = tmp;
+        return false;
     }
 }
