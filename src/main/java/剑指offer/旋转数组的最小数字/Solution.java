cddbfabed807e6ece2f41979b056d862b3aa80ce
euthpic <490114771@qq.com>
2022-05-19 18:55:48 +0800

    剑指offer.旋转数组的最小数字

--- src/main/java/剑指offer/旋转数组的最小数字/Solution.java
+++ src/main/java/剑指offer/旋转数组的最小数字/Solution.java
@@ -2,18 +2,16 @@ package 剑指offer.旋转数组的最小数字;
 
 public class Solution {
     public int minArray(int[] numbers) {
-
         int left = 0;
         int right = numbers.length - 1;
-
         while (left < right) {
             int mid = (left + right) / 2;
             if (numbers[mid] > numbers[right]) {
                 left = mid + 1;
             } else if (numbers[mid] < numbers[right]) {
-                right = mid ;
+                right = mid;
             } else {
-                right --;
+                right--;
             }
         }
         return numbers[left];
